97178d0a2ecea9f03aa8fcb81771215d67ae0193
phangjunyu <phangjunyu@gmail.com>
2018-12-08 07:52:23 +0800

    added workshop

---
+++ workshop/examples/truffle/swapTokenToEther.js
@@ -0,0 +1,94 @@
+/* global artifacts, web3 */
+/* eslint-disable no-underscore-dangle, no-unused-vars */
+const BN = require('bn.js');
+const moment = require('moment');
+
+const Network = artifacts.require('./KyberNetwork.sol');
+const NetworkProxy = artifacts.require('./KyberNetworkProxy.sol');
+const KNC = artifacts.require('./mockTokens/KyberNetworkCrystal.sol');
+const MANA = artifacts.require('./mockTokens/Mana.sol');
+
+function stdlog(input) {
+  console.log(`${moment().format('YYYY-MM-DD HH:mm:ss.SSS')}] ${input}`);
+}
+
+function tx(result, call) {
+  const logs = (result.logs.length > 0) ? result.logs[0] : { address: null, event: null };
+
+  console.log();
+  console.log(`   ${call}`);
+  console.log('   ------------------------');
+  console.log(`   > transaction hash: ${result.tx}`);
+  console.log(`   > contract address: ${logs.address}`);
+  console.log(`   > gas used: ${result.receipt.gasUsed}`);
+  console.log(`   > event: ${logs.event}`);
+  console.log();
+}
+
+module.exports = async (callback) => {
+  const accounts = web3.eth.accounts._provider.addresses;
+  const userWallet = accounts[4];
+  const ETH_ADDRESS = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+  let expectedRate;
+  let slippageRate;
+  let result;
+
+  // Set the instances
+  const NetworkProxyInstance = await NetworkProxy.at(NetworkProxy.address);
+  const KNCInstance = await KNC.at(KNC.address);
+  const MANAInstance = await MANA.at(MANA.address);
+
+  stdlog('- START -');
+  stdlog(`KyberNetworkProxy (${NetworkProxy.address})`);
+
+  stdlog(`KNC balance of ${userWallet} = ${web3.utils.fromWei(await KNCInstance.balanceOf(userWallet))}`);
+  stdlog(`MANA balance of ${userWallet} = ${web3.utils.fromWei(await MANAInstance.balanceOf(userWallet))}`);
+  stdlog(`ETH balance of ${userWallet} = ${web3.utils.fromWei(await web3.eth.getBalance(userWallet))}`);
+
+  // Approve the KyberNetwork contract to spend user's tokens
+  await KNCInstance.approve(
+    NetworkProxy.address,
+    web3.utils.toWei(new BN(1000000)),
+    { from: userWallet },
+  );
+  await MANAInstance.approve(
+    NetworkProxy.address,
+    web3.utils.toWei(new BN(1000000)),
+    { from: userWallet },
+  );
+
+  ({ expectedRate, slippageRate } = await NetworkProxyInstance.getExpectedRate(
+    KNC.address, // srcToken
+    ETH_ADDRESS, // destToken
+    web3.utils.toWei(new BN(100)), // srcQty
+  ));
+
+  result = await NetworkProxyInstance.swapTokenToEther(
+    KNC.address, // srcToken
+    web3.utils.toWei(new BN(100)), // srcAmount
+    expectedRate, // minConversionRate
+    { from: userWallet },
+  );
+  tx(result, 'KNC <-> ETH swapTokenToEther()');
+
+  ({ expectedRate, slippageRate } = await NetworkProxyInstance.getExpectedRate(
+    MANA.address, // srcToken
+    ETH_ADDRESS, // destToken
+    web3.utils.toWei(new BN(1000)), // srcQty
+  ));
+
+  result = await NetworkProxyInstance.swapTokenToEther(
+    MANA.address, // srcToken
+    web3.utils.toWei(new BN(1000)), // srcAmount
+    expectedRate, // minConversionRate
+    { from: userWallet },
+  );
+  tx(result, 'MANA <-> ETH swapTokenToEther()');
+
+  stdlog(`KNC balance of ${userWallet} = ${web3.utils.fromWei(await KNCInstance.balanceOf(userWallet))}`);
+  stdlog(`MANA balance of ${userWallet} = ${web3.utils.fromWei(await MANAInstance.balanceOf(userWallet))}`);
+  stdlog(`ETH balance of ${userWallet} = ${web3.utils.fromWei(await web3.eth.getBalance(userWallet))}`);
+
+  stdlog('- END -');
+  callback();
+};
---
+++ workshop/truffle.js
@@ -0,0 +1,86 @@
+/*
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
+ * function when declaring them. Failure to do so will cause commands to hang. ex:
+ * ```
+ * mainnet: {
+ *     provider: function() {
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
+ *     },
+ *     network_id: '1',
+ *     gas: 4500000,
+ *     gasPrice: 10000000000,
+ *   },
+ */
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const mnemonic = 'gesture rather obey video awake genuine patient base soon parrot upset lounge';
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      provider: new HDWalletProvider(mnemonic, 'http://localhost:8545', 0, 10),
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: 5777,
+      gas: 6721975,
+      gasPrice: 20000000000,
+      confirmations: 0,
+      timeoutBlocks: 50,
+      skipDryRun: true,
+    },
+    kovan: {
+      provider: new HDWalletProvider(mnemonic, 'http://localhost:8545', 0, 10),
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: 42,
+      gas: 7000000,
+      gasPrice: 20000000000,
+      confirmations: 0,
+      timeoutBlocks: 500,
+      skipDryRun: true,
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(mnemonic, 'http://localhost:8545', 0, 10),
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: 4,
+      gas: 6900000,
+      gasPrice: 25000000000,
+      confirmations: 1,
+      timeoutBlocks: 500,
+      skipDryRun: true,
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, 'http://localhost:8545', 0, 10),
+      host: '127.0.0.1',
+      port: 80,
+      network_id: 3,
+      gas: 7000000,
+      gasPrice: 20000000000,
+      confirmations: 0,
+      timeoutBlocks: 500,
+      skipDryRun: false,
+    },
+    mainnet: {
+      provider: new HDWalletProvider(mnemonic, 'http://localhost:8545', 0, 10),
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: 1,
+      gas: 7000000,
+      gasPrice: 20000000000,
+      confirmations: 0,
+      timeoutBlocks: 500,
+      skipDryRun: false,
+    },
+  },
+  solc: {
+    version: '0.4.18',
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+    evmVersion: 'byzantium',
+  },
+};
