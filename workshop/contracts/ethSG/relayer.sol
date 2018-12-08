84c1f4b29ab3955904a963312ecc9a641d2f4fea
phangjunyu <phangjunyu@gmail.com>
2018-12-08 11:24:32 +0800

    more skeleton

--- workshop/contracts/ethSG/relayer.sol
+++ workshop/contracts/ethSG/relayer.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.4.18;
 import "../KyberNetworkProxy.sol";
+import "../examples/Trade.sol" as tradeContract;
+import "./0xService.sol" as 0xService;
 
 contract Relayer{
-
+    //mapping of minRate to address of reserve
+    mapping(uint256 => address) kyberOB;
 
     event Rates(address indexed sender, uint minConversionRate, uint slippageRate);
 
@@ -48,16 +51,33 @@ contract Relayer{
 
     }
 
-    function post0xOB(){
-
-    }
-
     //
-    function decision(){
+    function decision(ERC20 srcToken, uint srcQty, ERC20 destToken) public {
         //check KyberOB
+        kyberOB = queryKyberOB(srcToken, srcQty, destToken)
         //check 0xOB
-        //compare
-        //if there is optimal, execute trade with desired DEX
-        //if both are NOT optimal, post trade on 0xOB
+        0xOB = 0xService.query0xOB(srcToken, srcQty, destToken)
+        //compare and returns 0, 1 or 2
+        uint result = compare(kyberOB, 0xOB)
+        switch(result){
+
+            //if 0, means execute Trade with Kyber DEX
+            case 0:
+                tradeContract.execSwap(srcToken, srcQty, destToken)
+                //a Swap Event will be emitted
+                break;
+
+            //if 1, means execute Trade with 0xOB
+            case 1:
+                0xService.execSwap(srcToken, srcQty, destToken)
+                break;
+
+            //if 2, means both 0 and 1 did not have the desired rates
+            //and the final option would be to post on 0x's orderbook
+            case 2:
+                //UI should prompt and ask if proceed
+                0xService.post0xOB(srcToken, srcQty, destToken)
+                break;
+        }
     }
 }
